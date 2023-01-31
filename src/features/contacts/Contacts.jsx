1fd17b81dbaa7d1cec95f814c87ca8db66b6244d
Agnieszkaa86 <agnieszka.janowska86@gmail.com>
2023-01-31 22:30:12 +0100

    add

--- src/features/contacts/Contacts.jsx
+++ src/features/contacts/Contacts.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, {useRef} from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { addContact, selectContacts } from "./ContactsSlice";
 
 export function Contacts() {
     const contacts = useSelector(selectContacts);
-
+    const inputRef = useRef(null);
+    
     console.log("contacts: ", contacts)
+    
     const dispatch = useDispatch();
+
+    const handleAddContact = () => {
+        const contactName = inputRef.current.value;
+        dispatch(addContact(contactName))
+    };
 
     return (
         <>
             <h1>Phonebook</h1>
+            <input type="text" ref={inputRef}/>
             <button onClick={() => dispatch(addContact())}>ADD CONTACT</button>
             <ul>
                 {contacts.map((contact) => (
